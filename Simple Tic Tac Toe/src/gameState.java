26fbebb8e188e984d7dd310242db7c06ef83bfc7
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-25 08:08:10 -0500

    Updated and optimized, Tic-Tac-Toe

--- Simple Tic Tac Toe/src/gameState.java
+++ Simple Tic Tac Toe/src/gameState.java
@@ -1,61 +1,63 @@
-/*
-public class gameState extends BoardX{
 
+public class gameState extends BoardX{
+    char[][] board = BoardX.getBoard();
     boolean space=false;
     boolean Xwin=false;
     boolean Owin=false;
     int Impossible=0;
-    int y=0;//规律数   step
-    //分别计数  'X' and 'O' count
-    int a = 0,b=0;
-        for (String svalue:getBoard
-             ) {
+    int y=0;
+
+    //step
+    //'X' and 'O' count
+    int a = 0, b = 0;
+        for (String svalue : board) {
         if ("X".equals(svalue))
             a++;
         if ("O".equals(svalue))
             b++;
     }
-    Impossible=a-b;
 
-        for (int i = 0; i < getBoard.length; i+=3) {
+    Impossible = a-b;
+
+        for (int i = 0; i < board.length; i+=3) {
         System.out.print("| ");
-        System.out.print(getBoard[i]+" "+getBoard[i+1]+" "+getBoard[i+2]);
+        System.out.print(board[i]+" "+board[i+1]+" "+board[i+2]);
         System.out.print(" |");
         System.out.println();
-        if ("_".equals(getBoard[i]) || "_".equals(getBoard[i+1])||"_".equals(getBoard[i+2]))
+        if ("_".equals(board[i]) || "_".equals(board[i+1])||"_".equals(board[i+2]))
             space=true;
-        //列判断 Column
+        //Column
         y=i/3;
-        if ("X".equals(getBoard[y]) && "X".equals(getBoard[y+3]) && "X".equals(getBoard[y+6])){
+        if ("X".equals(board[y]) && "X".equals(board[y+3]) && "X".equals(board[y+6])){
             Xwin=true;
         }
 
-        if ("O".equals(getBoard[y]) && "O".equals(getBoard[y+3]) && "O".equals(getBoard[y+6])){
+        if ("O".equals(board[y]) && "O".equals(board[y+3]) && "O".equals(board[y+6])){
             Owin=true;
         }
 
         //行判断 row
-        if ("X".equals(getBoard[i])&&"X".equals(getBoard[i+1]) && "X".equals(getBoard[i+2])){
+        if ("X".equals(board[i])&&"X".equals(board[i+1]) && "X".equals(board[i+2])){
             Xwin=true;
         }
 
-        if ("O".equals(getBoard[i])&&"O".equals(getBoard[i+1]) && "O".equals(getBoard[i+2])){
+        if ("O".equals(board[i])&&"O".equals(board[i+1]) && "O".equals(board[i+2])){
             Owin=true;
         }
     }
-    //对角线判断  Diagonal line
-        if ("X".equals(getBoard[0]) && "X".equals(getBoard[4]) && "X".equals(getBoard[8]))
-    Xwin=true;
-        if ("X".equals(getBoard[2]) && "X".equals(getBoard[4]) && "X".equals(getBoard[6]))
-    Xwin=true;
+        //Diagonal line
+        if ("X".equals(board[0]) && "X".equals(board[4]) && "X".equals(board[8]))
+            Xwin=true;
+        if ("X".equals(board[2]) && "X".equals(board[4]) && "X".equals(board[6]))
+            Xwin=true;
 
-        if ("O".equals(getBoard[0]) && "O".equals(getBoard[4]) && "O".equals(getBoard[8]))
-    Owin=true;
-        if ("O".equals(getBoard[2]) && "O".equals(getBoard[4]) && "O".equals(getBoard[6]))
-    Owin=true;
+        if ("O".equals(board[0]) && "O".equals(board[4]) && "O".equals(board[8]))
+            Owin=true;
+        if ("O".equals(board[2]) && "O".equals(board[4]) && "O".equals(board[6]))
+            Owin=true;
         System.out.println("---------");
 
-        if (Xwin == true && Owin == true || Math.abs(Impossible)>=2)
+        if (Xwin == true && Owin == true || Math.abs(Impossible) > = 2)
             System.out.println("Impossible");
         else {
         if (Xwin == true)
@@ -71,4 +73,3 @@ public class gameState extends BoardX{
     }
     
 }
-*/
