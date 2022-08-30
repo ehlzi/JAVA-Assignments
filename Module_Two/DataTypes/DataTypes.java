d3dcec700d06e5801e46443b427a1bdaac12a775
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-30 18:35:44 -0500

    Create DataTypes.java

---
+++ Module_Two/DataTypes/DataTypes.java
@@ -0,0 +1,154 @@
+class Main {
+  
+  public static void main(String[] args) {
+    
+    String theirResults = "Their Results: ";
+    String myResults = "My Results: ";
+    String exOne = "***Excercise One: ";
+    String exTwo = "***Excercise Two: ";
+    String exThree = "***Excercise Three: ";
+    String exFour = "***Excercise Four: ";
+    String exFive = "***Excercise Five: ";
+    String exSix = "***Excercise Six: ";
+    String exSeven = "***Excercise Seven: ";
+    String exEight = "***Excercise Eight: ";
+    String exNine = "***Excercise Nine: ";
+    String exTen = "***Excercise Ten: ";
+    String exEleven = "***Excercise Eleven: ";
+    String exTwelve = "***Excercise Twelve: ";
+
+    System.out.print(exOne + "\n");
+    
+//-------------------------------------------
+    System.out.print(theirResults);
+    byte aByte = -128;
+    System.out.println(aByte + "\n");
+//-------------------------------------------
+    
+    System.out.print(myResults);
+    byte myFirstByte = 127;
+    System.out.println(myFirstByte+ "\n");
+    System.out.print(exTwo+ "\n");
+
+//-------------------------------------------
+    
+    System.out.print(myResults);
+    byte mySecondByte = -128;
+    System.out.println(mySecondByte + "\n");
+    System.out.print(exThree + "\n");
+    
+//-------------------------------------------
+
+    System.out.print(theirResults);
+    short aShort = 32766;
+    System.out.println(aShort + "\n");
+    
+//-------------------------------------------
+    System.out.print(theirResults);
+    int anInt = -56;
+    System.out.println(anInt + "\n");
+    
+//-------------------------------------------
+
+    String myIntAnswer = "My Code: 'int myInt = -2147483648;'";
+    System.out.print(myIntAnswer + "\n");
+    int myInt = -2147483648;
+    System.out.println();
+    System.out.print(exFour + "\n");
+
+//-------------------------------------------
+
+    System.out.print(myResults + "\n");
+    System.out.println(myInt + "\n");
+    System.out.print(exFive);
+    
+//-------------------------------------------
+    System.out.print(theirResults);
+    long aLong = 9223375807L;
+    System.out.println(aLong + "\n");
+    
+//-------------------------------------------  
+    
+    System.out.print(theirResults);
+    float aFloat = 9.99f;
+    System.out.println(aFloat + "\n");
+    
+//-------------------------------------------
+
+    String myFloatAnswer = "My Code: 'float myFloat = 1.0111f;'";
+    System.out.print(myFloatAnswer + "\n");
+    float myFloat = 1.0111f;
+    System.out.println();
+    System.out.print(exSix + "\n");
+
+//-------------------------------------------
+
+    System.out.print(myResults + "\n");
+    System.out.println(myFloat + "\n");
+    System.out.print(exSeven + "\n");
+    
+//-------------------------------------------
+
+    System.out.print(theirResults);
+    double aDouble = 99.98d;
+    System.out.println(aDouble + "\n");
+    System.out.print(theirResults);
+    boolean aBoolean = true;
+    System.out.println(aBoolean + "\n");
+    
+//-------------------------------------------
+
+    String myBooleanAnswer = "My Code: 'boolean myBoolean = false;'";
+    System.out.println(myBooleanAnswer + "\n");
+    boolean myBoolean = false;
+    System.out.println();
+    System.out.print(exEight + "\n");
+
+//-------------------------------------------
+    
+    System.out.print(myResults + "\n");
+    System.out.println(myBoolean + "\n");
+    System.out.print(exNine);
+    System.out.println();
+    
+//-------------------------------------------
+
+    System.out.print(theirResults);
+    char aChar = 'a';
+    System.out.println(aChar);
+    System.out.println();
+//-------------------------------------------
+
+    String myCharAnswer = "My Code: 'char myChar = 1;'";
+    System.out.print(myCharAnswer + "\n");
+    System.out.println();
+    char myChar = '1';
+    System.out.print(exTen + "\n");
+    
+//-------------------------------------------
+
+    System.out.print(myResults + "\n");
+    System.out.println(myChar + "\n");
+    System.out.print(exEleven + "\n");
+    
+//-------------------------------------------
+
+    System.out.print(theirResults);
+    String aString = "the quick brown fox jumps over the lazy dog";
+    System.out.println(aString);
+    System.out.println();
+//-------------------------------------------
+
+    String myStringAnswer = "My Code: 'string I mastered Java data types!;'";
+    System.out.print(myStringAnswer + "\n");
+    String myString = "I mastered Java data types!";
+    System.out.print(exTwelve + "\n");
+
+//-------------------------------------------
+
+    System.out.print(myString + "\n");
+    System.out.println(myString);
+    System.out.println();
+
+  }
+}
