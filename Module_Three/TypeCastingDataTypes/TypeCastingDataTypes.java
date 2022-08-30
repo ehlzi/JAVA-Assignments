f95af3554c2d5725e08633963f7fc285f0499081
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-30 18:58:36 -0500

    Create TypeCastingDataTypes.java

---
+++ Module_Three/TypeCastingDataTypes/TypeCastingDataTypes.java
@@ -0,0 +1,20 @@
+class TypeCastingDataTypes {
+  
+  public static void main(String[] args) {
+
+    float deposit = 6.50f;
+
+    int balance = 0;
+
+    int z = 10;
+
+    float y = 3.755f;
+
+    balance = balance + (int) deposit;
+
+    System.out.println("Mary started with: " + deposit + ". \n This is the amount the bank registered: " + balance);
+    
+    System.out.println(z + y);
+    
+  }
+}
