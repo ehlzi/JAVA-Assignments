5097e06d3a1ac5ba9d262bc660b2876802102992
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-30 18:36:52 -0500

    Create Variables.java

---
+++ Module_Two/Variables/Variables.java
@@ -0,0 +1,18 @@
+class Variables {
+  
+  public static void main(String[] args) {
+    
+      int myInt = 10;
+    
+      System.out.println(myInt);
+
+      String myString = "Hello World!";
+
+      System.out.println(myString);
+    
+      System.out.println("15");
+
+      System.out.println("This is a string");
+    
+  }
+}
