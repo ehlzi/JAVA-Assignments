b66602ddee7499c80b9e19274b02b3fc536542f8
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-30 19:21:21 -0500

    Update and rename Temp.java to Classes.java

---
+++ Module_Four/Classes/Classes.java
@@ -0,0 +1,13 @@
+class Classes {
+  
+  public static void main(String[] args) {
+    
+    Dog dog1 = new Dog();
+    
+    System.out.println("Hello World!");
+    
+    dog1.greeting();
+    
+  }
+
+}
