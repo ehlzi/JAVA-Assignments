172324badefbca861b4f52dc8bb808b8c4011529
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-30 19:03:34 -0500

    Create Classes.java

---
+++ Module_Four/Classes.java
@@ -0,0 +1,14 @@
+class Classes {
+  
+  public static void main(String[] args) {
+    
+    Dog dog1 = new Dog();
+    
+    System.out.println("Hello World!");
+    
+    dog1.greeting();
+    
+  }
+
+}
+
