668d3a7805fb2dba0b2bbb94794daff4246e57a6
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-09-02 03:11:35 -0500

    More backups of projects!

--- Module_2/DataTypes/DataTypes.java
+++ Module_2/DataTypes/DataTypes.java
@@ -3,6 +3,8 @@ class DataTypes {
   public static void main(String[] args) {
     
     String theirResults = "Their Results: ";
+
+    "if im coding here"
     String myResults = "My Results: ";
     String exOne = "***Excercise One: ";
     String exTwo = "***Excercise Two: ";
